de6b894f9e35676b31ead35184c4760fb71453bd
root <root@ip-172-30-0-177.eu-west-1.compute.internal>
2020-07-16 00:01:22 +0000

    temp_3

--- test/print.py
+++ test/print.py
@@ -10,7 +10,7 @@ r4 = redis.StrictRedis(host='34.254.60.203', port=6379, db=4)
 
 
 print("\n\n")
-print(r1.set('sql_freq','INFREQUENT'))
-print(r2.set('sql_count',3))
+print(r1.set('sql_freq','NEW'))
+print(r2.set('sql_count',1))
 print(r3.delete('sql_cpath'))
 print(r3.get('sql_last_used'))
